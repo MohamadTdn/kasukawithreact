f1d76e61258327ff7ccdd722e3c9bed15e26b350
MohamadTdn <mohamadmt1579@gmail.com>
2024-05-10 18:19:03 +0430

    add features component in header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css';
 import Navbar from './Navbar/Navbar'
+import Features from './Features/Features'
 
 function App() {
   return (
@@ -8,6 +9,13 @@ function App() {
       <div className='Header'>
         <Navbar></Navbar>
         <h1 className="Header-title">راه حل های قدرتمند <br /> دیجیتال با <br /> <span>Kasuka</span></h1>
+        <h4 className='Header-subtitle'>ما تیمی از بازاریابان با استعداد دیجیتال هستیم</h4>
+        <div className='Features-container'>
+          <Features></Features>
+          <Features></Features>
+          <Features></Features>
+          <Features></Features>
+        </div>
       </div>
     </div>
   );
---
+++ src/Features/Features.js
@@ -0,0 +1,11 @@
+import './Features.css'
+
+function Features () {
+    return (
+        <div className='Features'>
+            <h1 className='Features-title'>متن تستی</h1>
+        </div>
+    )
+}
+
+export default Features
