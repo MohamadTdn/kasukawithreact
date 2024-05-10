a7d20a59cff69b7131c6d4fcfefd82e652272234
MohamadTdn <mohamadmt1579@gmail.com>
2024-05-10 18:36:50 +0430

    add company component

---
+++ src/Company/Company.js
@@ -0,0 +1,11 @@
+import './Company.css'
+
+function Company () {
+    return (
+        <div className="Company">
+            <img src="mit.png" alt="" />
+        </div>
+    )
+}
+
+export default Company
